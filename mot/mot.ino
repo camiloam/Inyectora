e0548f376077dfb8fc52d7bd5310841293464acd
camilo <camilo.a130@gmail.com>
2021-06-20 16:42:16 -0500

    Actualización

--- mot/mot.ino
+++ mot/mot.ino
@@ -15,9 +15,17 @@ enum screen {inicio, plasticos, control, datos} pantalla = inicio;
 uint8_t const color_1[] = {102, 219, 255};
 uint8_t const color_2[] = {29, 169, 245};
 uint8_t const color_3[] = {54, 208, 255};
+
+uint8_t* const list_plasticos[] = {"P1","P2","P3","P4","P5","P6","P7","P8","P9","P10","P11","P12","P13",
+                              "P14","P15","P16","P17","P18","P19","P20","P21","P22","P23","P24","P25"};
+//uint16_t const negro = 0, blanco = 0xFFFF;
+enum colores {negro = 0x0000, blanco = 0xFFFF};
+
+                              
+
 uint16_t WIDTH;
 uint16_t HEIGHT;
-uint8_t str = "";
+uint8_t str = 0;
 
 void setup() {
   // Sets the two pins as Outputs
@@ -27,10 +35,11 @@ void setup() {
 
   digitalWrite(dirPin, HIGH); //Enables the motor to move in a particular direction
 
-  Serial.begin(9600);
-  my_lcd.Init_LCD();
-  Serial.println(my_lcd.Read_ID(), HEX);
+  my_lcd.Init_LCD(); 
   my_lcd.Set_Rotation(1);
+  Serial.begin(9600);
+  Serial.println(my_lcd.Read_ID(), HEX);
+  
 }
 
 
@@ -40,63 +49,89 @@ void loop() {
 
   if (pantalla == inicio) {
     my_lcd.Fill_Screen(conv_color(color_2));
-    show_string((uint8_t*)"ChemiTech", CENTER, 50, 4, 0xFFFF);
+    show_string((uint8_t*)"ChemiTech", CENTER, 50, 4, blanco);
 
     my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
-    Rectangle(WIDTH / 2 - 100, 120, WIDTH / 2 + 100, 170, (uint8_t*)"INYECTORA", 2, 0xFFFF);
-    Rectangle(WIDTH / 2 - 100, 220, WIDTH / 2 + 100, 270, (uint8_t*)"BASE DE DATOS", 2, 0xFFFF);
+    Rectangle(WIDTH / 2 - 100, 120, WIDTH / 2 + 100, 170, (uint8_t*)"INYECTORA", 2, blanco);
+    Rectangle(WIDTH / 2 - 100, 220, WIDTH / 2 + 100, 270, (uint8_t*)"BASE DE DATOS", 2, blanco);
 
     while (pantalla == inicio) {
 
+      pantalla = plasticos;
+
       str = readS();
-      if (str == '+') {
+      if (str == '0') {
+        pantalla = inicio;
+      }
+      else if (str == '1') {
+        pantalla = plasticos;
+      }
+      else if (str == '2') {
         pantalla = control;
       }
-      else if (str == '-') {
+      else if (str == '3') {
         pantalla = datos;
-      }
-      else if (str == '0') {
-        pantalla = inicio;
       }
     }
 
   }
 
   if (pantalla == plasticos) {
-    my_lcd.Fill_Screen(0xFFFF);
-    show_string("Les plastiques", 0, 0, 2, 0);
+    my_lcd.Fill_Screen(blanco);
+    my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
+    my_lcd.Fill_Rectangle(0, 0, WIDTH, 25);
+    show_string("ChemiTech", WIDTH - 120, 5, 2, blanco);
+    
+    show_string("Elija el plastico:", 10, 50, 2, negro);
+
+    my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
+    my_lcd.Fill_Rectangle(10, 75, WIDTH - 10, HEIGHT - 10);
+
+    
+
+    
+
+    
+
+    
 
     while (pantalla == plasticos) {
 
       str = readS();
-      if (str == '+') {
+      if (str == '0') {
+        pantalla = inicio;
+      }
+      else if (str == '1') {
+        pantalla = plasticos;
+      }
+      else if (str == '2') {
         pantalla = control;
       }
-      else if (str == '-') {
+      else if (str == '3') {
         pantalla = datos;
-      }
-      else if (str == '0') {
-        pantalla = inicio;
       }
     }
 
   }
 
   if (pantalla == control) {
-    my_lcd.Fill_Screen(0xFFFF);
+    my_lcd.Fill_Screen(blanco);
     show_string("Le control", 0, 0, 2, 0);
 
     while (pantalla == control) {
 
       str = readS();
-      if (str == '+') {
+      if (str == '0') {
+        pantalla = inicio;
+      }
+      else if (str == '1') {
+        pantalla = plasticos;
+      }
+      else if (str == '2') {
         pantalla = control;
       }
-      else if (str == '-') {
+      else if (str == '3') {
         pantalla = datos;
-      }
-      else if (str == '0') {
-        pantalla = inicio;
       }
     }
 
@@ -109,14 +144,17 @@ void loop() {
     while (pantalla == datos) {
 
       str = readS();
-      if (str == '+') {
+      if (str == '0') {
+        pantalla = inicio;
+      }
+      else if (str == '1') {
+        pantalla = plasticos;
+      }
+      else if (str == '2') {
         pantalla = control;
       }
-      else if (str == '-') {
+      else if (str == '3') {
         pantalla = datos;
-      }
-      else if (str == '0') {
-        pantalla = inicio;
       }
     }
     
@@ -172,7 +210,7 @@ void show_string(uint8_t *str, int16_t x, int16_t y, uint8_t csize, uint16_t fc)
 
 
 uint8_t readS() {
-  uint8_t c = "";
+  uint8_t c = 0;
   if (Serial.available()) {
     c = Serial.read();
   }

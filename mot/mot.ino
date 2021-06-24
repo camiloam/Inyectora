f0bbbe412576fc09e0c844e5f95fe47aeddc69a6
camilo <camilo.a130@gmail.com>
2021-06-24 18:28:59 -0500

    Pantallas 1 y 2 terminadas

--- mot/mot.ino
+++ mot/mot.ino
@@ -16,17 +16,26 @@ uint8_t const color_1[] = {102, 219, 255};
 uint8_t const color_2[] = {29, 169, 245};
 uint8_t const color_3[] = {54, 208, 255};
 
-uint8_t* const list_plasticos[] = {"Poliacetal","Acrilico","ABS","Acetato de celulosa","PTFE","Nylon-6",
-                                    "Policarbonato","PET","Polietileno (baja densidad)","Polietileno (alta densidad)",
-                                    "Polipropileno","Poliestireno","PVC"};
+uint8_t* const list_plasticos[] = {"Poliacetal", "Acrilico", "ABS", "Acetato de celulosa", "PTFE", "Nylon-6",
+                                   "Policarbonato", "PET", "Polietileno (baja densidad)", "Polietileno (alta densidad)",
+                                   "Polipropileno", "Poliestireno", "PVC", "Plasx","Plasxx","Plasxxx","Plasxxxx"
+                                  };
 //uint16_t const negro = 0, blanco = 0xFFFF;
 enum colores {negro = 0x0000, blanco = 0xFFFF};
-
-                              
 
 uint16_t WIDTH;
 uint16_t HEIGHT;
 uint8_t str = 0;
+
+int const tabla_y0 = 80;
+int const plastY = 33;
+int const plast_vis = 7;
+int const plast_tot = sizeof(list_plasticos) / sizeof(list_plasticos[0]);
+int const plast_y0 = tabla_y0 + (plastY - 7 * 2) / 2 ;
+int const scrollY = plast_vis < plast_tot ? plast_vis * plast_vis * plastY / plast_tot : 0;
+
+
+
 
 void setup() {
   // Sets the two pins as Outputs
@@ -36,12 +45,15 @@ void setup() {
 
   digitalWrite(dirPin, HIGH); //Enables the motor to move in a particular direction
 
-  my_lcd.Init_LCD(); 
+  my_lcd.Init_LCD();
   my_lcd.Set_Rotation(1);
   Serial.begin(9600);
   Serial.println(my_lcd.Read_ID(), HEX);
-  
+
 }
+
+
+
 
 
 void loop() {
@@ -78,25 +90,25 @@ void loop() {
   }
 
   if (pantalla == plasticos) {
+
+    int item  = 0;
+    int postext = 0;
+    int scroll = 0;
+
     my_lcd.Fill_Screen(blanco);
     my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
     my_lcd.Fill_Rectangle(0, 0, WIDTH, 25);
     show_string("ChemiTech", WIDTH - 120, 5, 2, blanco);
-    
-    show_string("Elija el plastico:", 10, 50, 2, negro);
-    my_lcd.Set_Draw_color(color_3[0], color_3[1], color_3[2]);
-    my_lcd.Fill_Rectangle(10, 75, WIDTH - 10, HEIGHT - 10);
-    
-    my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
-    my_lcd.Fill_Rectangle(10, 75, WIDTH - 10, 104);  
-    for (int i = 0; i < sizeof(list_plasticos)/sizeof(list_plasticos[0]) ; i++){
-       show_string(list_plasticos[i], 20, 81 + 30*i, 2, blanco);
-    }
-    my_lcd.Set_Draw_color(color_2[0], color_2[1], color_2[2]);
-    my_lcd.Fill_Rectangle(WIDTH - 15, 75, WIDTH - 10, 150);
-    
 
-    item  = 0;
+    show_string("Elija el plastico:", 20, 45, 2, negro);
+
+    my_lcd.Set_Draw_color(color_3[0], color_3[1], color_3[2]);
+    my_lcd.Fill_Rectangle(10, tabla_y0, WIDTH - 10, HEIGHT - 10);
+
+    my_lcd.Set_Draw_color(color_3[0], color_3[1], color_3[2]);
+    my_lcd.Fill_Rectangle(10, tabla_y0 + plastY * item, WIDTH - 10, tabla_y0 + plastY * (item + 1) - 1);
+    Actualizar_plasticos(item, postext, scroll);
+
     while (pantalla == plasticos) {
 
       str = readS();
@@ -113,22 +125,41 @@ void loop() {
         pantalla = datos;
       }
 
-      if (str == '+'){
-        if (item > 0){
+      if (str == '+') {
+        my_lcd.Set_Draw_color(color_3[0], color_3[1], color_3[2]);
+        if (item < plast_vis - 1) {
+          my_lcd.Fill_Rectangle(10, tabla_y0 + plastY * item, WIDTH - 10, tabla_y0 + plastY * (item + 1) - 1);
           item += 1;
         }
-        Actualizar_plasticos();
-      }
+        else if (postext < plast_tot - plast_vis) {
 
-      if (str == '+'){
-        if (item > 0){
-          item += 1;
+          for (int i = postext; i < plast_vis + postext ; i++) {
+            show_string(list_plasticos[i], 20, plast_y0 + plastY * (i - postext), 2, conv_color(color_3));
+          }
+          my_lcd.Fill_Rectangle(WIDTH - 15, tabla_y0 + scroll, WIDTH - 10, tabla_y0 + scrollY + scroll - 1);
+          postext += 1;
+          scroll = (plast_vis * plastY - scrollY) * postext / (plast_tot - plast_vis);
         }
-        Actualizar_plasticos();
+        Actualizar_plasticos(item, postext, scroll);
       }
 
+      if (str == '-') {
+        my_lcd.Set_Draw_color(color_3[0], color_3[1], color_3[2]);
+        if (item > 0) {
+          my_lcd.Fill_Rectangle(10, tabla_y0 + plastY * item, WIDTH - 10, tabla_y0 + plastY * (item + 1) - 1);
+          item -= 1;
+        }
+        else if (postext > 0) {
+          for (int i = postext; i < plast_vis + postext ; i++) {
+            show_string(list_plasticos[i], 20, plast_y0 + plastY * (i - postext), 2, conv_color(color_3));
+          }
+          my_lcd.Fill_Rectangle(WIDTH - 15, tabla_y0 + scroll, WIDTH - 10, tabla_y0 + scrollY + scroll - 1);
+          postext -= 1;
+          scroll = plast_vis * plastY * postext / plast_tot;
+        }
+        Actualizar_plasticos(item, postext, scroll);
+      }
 
-      
     }
 
   }
@@ -176,7 +207,7 @@ void loop() {
         pantalla = datos;
       }
     }
-    
+
   }
 
 
@@ -233,7 +264,9 @@ uint8_t readS() {
   if (Serial.available()) {
     c = Serial.read();
   }
-  Serial.println(c);
+  if (c != 0) {
+    Serial.println(c);
+  }
   return c;
 }
 
@@ -241,18 +274,20 @@ void Rectangle(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t *str, uin
 {
   my_lcd.Fill_Rectangle(x1, y1, x2, y2);
   uint16_t len = strlen((const char *)str) * 6 * csize;
-  int16_t posx = x2 - x1 + 1 - len;
-  int16_t posy = y2 - y1 + 1 - 7 * csize;
-  show_string(str, x1 + posx / 2, y1 + posy / 2, csize, cText);
-  show_string(str, x1 + posx / 2, y1 + posy / 2, csize, cText);
+  int16_t posx = x1 + (x2 - x1 + 1 - len) / 2;
+  int16_t posy = y1 + (y2 - y1 + 1 - 7 * csize) / 2;
+  show_string(str, posx, posy, csize, cText);
 }
 
-void Actualizar_plasticos(int item){
+void Actualizar_plasticos(int item, int postext, int scroll) {
+  //pintar fila
   my_lcd.Set_Draw_color(color_1[0], color_1[1], color_1[2]);
-  my_lcd.Fill_Rectangle(10, 75 + 30*item, WIDTH - 10, 104);  
-  for (int i = 0; i < sizeof(list_plasticos)/sizeof(list_plasticos[0]) ; i++){
-     show_string(list_plasticos[i], 20, 81 + 30*i, 2, blanco);
-  }
+  my_lcd.Fill_Rectangle(10, tabla_y0 + plastY * item, WIDTH - 10, tabla_y0 + plastY * (item + 1) - 1);
+  //pintar scroll
   my_lcd.Set_Draw_color(color_2[0], color_2[1], color_2[2]);
-  my_lcd.Fill_Rectangle(WIDTH - 15, 75, WIDTH - 10, 150);
+  my_lcd.Fill_Rectangle(WIDTH - 15, tabla_y0 + scroll, WIDTH - 10, tabla_y0 + scrollY + scroll - 1);
+  //llenar plasticos
+  for (int i = postext; i < plast_vis + postext ; i++) {
+    show_string(list_plasticos[i], 20, plast_y0 + plastY * (i - postext), 2, blanco);
+  }
 }
